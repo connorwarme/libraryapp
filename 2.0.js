df538d633528e7f79f6f459750fbecd60a2647ee
connorwarme <connor.warme@gmail.com>
2022-02-10 17:38:56 -0700

    adding modal functionality

--- 2.0.js
+++ 2.0.js
@@ -6,9 +6,6 @@ const Book = function(title, author, pages, read, dataValue) {
     this.pages = pages;
     this.read = read;
     this.dataValue = `-1`;
-    // const addToLibrary = (book) => {
-    // book.prototype = Object.create(Book.prototype);
-    // myLibrary.push(book)};
     const display = function() {
         const card = document.createElement('div');
         card.classList.add('bookCard');
@@ -65,51 +62,11 @@ const Book = function(title, author, pages, read, dataValue) {
         const libCard = document.getElementById(`${this.dataValue}`);
         cardContainer.removeChild(libCard);
     }
-    return {title, author, genre, pages, read, dataValue, updateRead, deleteCard, display};
+    const addToLibrary = function() {
+        myLibrary.push(this);
+    }
+    return {title, author, pages, read, dataValue, updateRead, deleteCard, display, addToLibrary};
 }
-// create book prototype
-Object.prototype.addToLibrary = function() {
-    myLibrary.push(this);
-}
-// Object.prototype.display = function() {
-    // const card = document.createElement('div');
-    // card.classList.add('bookCard');
-    // // card.setAttribute('id', `${i}`);
-    // cardContainer.appendChild(card);
-    // const title = document.createElement('div');
-    // title.textContent = `${this.title}`;
-    // title.classList.add('cardTitle');
-    // card.appendChild(title);
-    // const author = document.createElement('div');
-    // author.textContent = `${this.author}`;
-    // author.classList.add('cardAuthor');
-    // card.appendChild(author);
-    // // const genre = document.createElement('div');
-    // // genre.textContent = `${this.genre}`;
-    // // genre.classList.add('cardGenre');
-    // // card.appendChild(genre);
-    // const pages = document.createElement('div');
-    // pages.textContent = `${this.pages} pages`;
-    // pages.classList.add('cardPages');
-    // card.appendChild(pages);
-    // const readLabel = document.createElement('label');
-    // readLabel.setAttribute('for', 'read');
-    // readLabel.classList.add('toggle');
-    // readLabel.textContent = `${this.read}`;
-    // card.appendChild(readLabel);
-    // const removeContainer = document.createElement('div');
-    // removeContainer.classList.add('removeContainer')
-    // card.appendChild(removeContainer);
-    // const remove = document.createElement('button');
-    // remove.classList.add('remove');
-    // remove.textContent = `Delete`;
-    // removeContainer.appendChild(remove);
-    // const number = document.createElement('div');
-    // number.classList.add('number');
-    // // number.textContent = `#${i + 1}`;
-    // removeContainer.appendChild(number);
-    // }
-
 let book1 = new Book('A Very Punchable Face', 'Colin Jost', 346, 'Read');
 let book2 = new Book('Emperor of All Maladies', 'Siddhartha Mukherjee', 608, 'Unread');
 let book3 = new Book('Zero Fail', 'Carol Leonig', 561, 'Read');
@@ -151,70 +108,108 @@ const listener = function() {
         deleteButton[index].addEventListener('click', (e) => {
             let x = e.target.parentNode.parentNode;
             runItBack(x.id);
+        }) 
+    })
+    const readLabel = Array.from(document.querySelectorAll('label.toggle'));
+    readLabel.forEach(function(part, index) {
+        readLabel[index].addEventListener('click', (e) => {
+            let x = e.target.parentNode;
+            myLibrary[x.id].updateRead();
         })
-        
     })
 }
+// modal listeners / functions
+const modalListener = () => {
+    const modal = document.querySelector('div.addmodal');
+    const addButton = document.querySelector('button.add');
+    addButton.addEventListener('click', e => {
+        modal.style.display = "block";
+    })
+    const closeBtn = document.querySelector('span.close');
+    closeBtn.addEventListener('click', e => {
+        modal.style.display = "none";
+        clearForm();
+    })
+    const resetBtn = document.querySelector('input.reset');
+    resetBtn.addEventListener('click', e => {
+        clearForm();
+    })
+    const userInput = document.querySelectorAll('input.form');
+    const clearForm = () => {
+        userInput.forEach(form => form.value = ``);
+    }
+    const inLibrary = function(input) {
+        console.log(input);
+        let check = myLibrary.find(book => book.title == input.title);
+        console.log(check);
+        if (check == undefined) {
+            input.addToLibrary();
+            console.log(myLibrary);
+        } else {
+            alert(`no bueno`);
+        }
+    }
+    let inputValues = [];
+    const concatForm = () => {
+        userInput.forEach(function(part, index) {
+            inputValues.push(`${userInput[index].value}`);
+        })
+            console.log(inputValues);
+    }
+    const submitBtn = document.querySelector('input.submit');
+    submitBtn.addEventListener('click', e => {
+        concatForm();
+        const newBook = new Book(inputValues[0], inputValues[1], inputValues[2], inputValues[3]);
+        console.log(inLibrary(newBook));
+        modal.style.display = "none";
+        // displayLibrary();
+        clearForm();
+        inputValues = [];
+    })
+}
+// const inLibrary = function(input) {
+//     let x = 0;
+//     for (i=0; i<library.length; i++) {
+//       if (library[i].title === input.title) {
+//           x++;
+//     return alert(`That book is already in the library!`);
+//     }}
+//     if (x == 0) {
+//     bookAdd(input);
+//     console.log(`book add`);
+//     }
+// }
+// const submitBtn = document.querySelector('input.submit');
+// submitBtn.addEventListener('click', e => {
+//     concatForm();
+//     const newTitle = new book(inputValues[0], inputValues[1], inputValues[2], inputValues[3], inputValues[4],);
+//     inLibrary(newTitle);
+//     modal.style.display = "none";
+//     display();
+//     clearForm();
+//     inputValues = [];
+// })
+// const resetBtn = document.querySelector('input.reset');
+// resetBtn.addEventListener('click', e => {
+//     clearForm();
+// })
+// const userInput = document.querySelectorAll('input.form');
+// const clearForm = function() {
+//     userInput.forEach(function(part, index) {
+//     userInput[index].value = '';
+//     })
+// }
+// let inputValues = [];
+// const concatForm = function() {
+//     userInput.forEach(function(part, index) {
+//     inputValues.push(`${userInput[index].value}`);
+//     })
+// }
 const runItBack = function(input) {
     myLibrary[input].deleteCard();
     updateLibrary(input);
     updateNumber();
-    console.log('you did it..?');
 }
-// const findCard =  myLibrary.filter(x => x == this.dataValue);
-
-// loop thru array & display
-
-// const cardContainer = document.querySelector('div.cardContainer');
-// const displayLibrary = () => {
-//     for (i=0; i<myLibrary.length; i++) {
-//     const card = document.createElement('div');
-//     card.classList.add('bookCard');
-//     card.setAttribute('id', `${i}`);
-//     cardContainer.appendChild(card);
-//     const title = document.createElement('div');
-//     title.textContent = `${myLibrary[i].title}`;
-//     title.classList.add('cardTitle');
-//     card.appendChild(title);
-//     const author = document.createElement('div');
-//     author.textContent = `${myLibrary[i].author}`;
-//     author.classList.add('cardAuthor');
-//     card.appendChild(author);
-//     const genre = document.createElement('div');
-//     genre.textContent = `${myLibrary[i].genre}`;
-//     genre.classList.add('cardGenre');
-//     card.appendChild(genre);
-//     const pages = document.createElement('div');
-//     pages.textContent = `${myLibrary[i].pages} pages`;
-//     pages.classList.add('cardPages');
-//     card.appendChild(pages);
-//     const year = document.createElement('div');
-//     year.textContent = `${myLibrary[i].year}`;
-//     year.classList.add('cardYear');
-//     card.appendChild(year);
-//     const read = document.createElement('input');
-//     read.classList.add('read');
-//     read.setAttribute('type', 'checkbox');
-//     read.setAttribute('id', 'unread');
-//     read.setAttribute('name', `read`)
-//     card.appendChild(read);
-    // const readLabel = document.createElement('label');
-    // readLabel.setAttribute('for', 'read');
-    // readLabel.classList.add('toggle');
-    // readLabel.textContent = `Unread`;
-    // card.appendChild(readLabel);
-    // const removeContainer = document.createElement('div');
-    // removeContainer.classList.add('removeContainer')
-    // card.appendChild(removeContainer);
-    // const remove = document.createElement('button');
-    // remove.classList.add('remove');
-    // remove.textContent = `Delete`;
-    // removeContainer.appendChild(remove);
-    // const number = document.createElement('div');
-    // number.classList.add('number');
-    // number.textContent = `#${i + 1}`;
-    // removeContainer.appendChild(number);
-//     }
-// }
 displayLibrary();
 listener();
+modalListener();
